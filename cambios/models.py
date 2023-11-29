04780c5b9385138f9d24e4e8e8db9cbffb43a210
benjacegarra <benjamin.cegarra@gmail.com>
2023-11-29 09:17:35 -0300

    base de datos

--- cambios/models.py
+++ cambios/models.py
@@ -6,3 +6,9 @@ class Informacion(models.Model):
     email=models.EmailField(null=False, max_length=30, default="default@gmail.com")
      
 # Create your models here.
+class Votaciones(models.Model):
+    creador=models.CharField(null=False, max_length=50)
+    ciudad=models.CharField(null=False, max_length=50)
+    direccion=models.CharField(null=False, max_length=50)
+    radio=models.DecimalField(null=False,max_digits=3, decimal_places=0)
+    descripcion=models.CharField(null=False, max_length=10)
